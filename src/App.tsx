62103005757d1bfceecb46a3eeccd0f3c5d84982
chetan0121 <prajapatichetandilipbhai@gmail.com>
2026-04-01 18:48:13 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
+import Home from './pages/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div id="App" className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-3xl font-bold text-blue-500">
-        React + TypeScript + Tailwind Template
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
